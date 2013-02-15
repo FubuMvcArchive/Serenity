cbcd711cdc93c26ea0098c0c65a7165788b8edf8
jmarnold <rncodepoet@gmail.com>
2013-02-15 13:50:51 -0600

    Stupid gitignore

---
+++ src/Serenity/StoryTeller/StoryTellerCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel;
+using FubuCore;
+using FubuCore.CommandLine;
+using StoryTeller;
+using StoryTeller.Execution;
+
+namespace Serenity.StoryTeller
+{
+	public class StoryTellerInput
+	{
+		public StoryTellerInput()
+		{
+			BrowserFlag = BrowserType.Firefox;
+		}
+
+		[Description("Path to the StoryTeller project file")]
+		public string ProjectFile { get; set; }
+
+		[Description("Path to write out the results")]
+		public string ResultsFile { get; set; }
+
+		[Description("Optional.  Runs only one workspace")]
+		public string WorkspaceFlag { get; set; }
+
+		[Description("Choose which browser to use for the testing")]
+		public BrowserType BrowserFlag { get; set; }
+	}
+
+	[CommandDescription("Run a suite of StoryTeller tests")]
+	public class StoryTellerCommand : FubuCommand<StoryTellerInput>
+	{
+		public override bool Execute(StoryTellerInput input)
+		{
+			StoryTellerEnvironment.Set(new SerenityEnvironment
+			{
+				Browser = input.BrowserFlag,
+				WorkingDir = AppDomain.CurrentDomain.BaseDirectory
+			});
+
+			Console.WriteLine("Using browser " + input.BrowserFlag);
+
+			var runner = new ProjectRunner(new[] { input.ProjectFile }, input.ResultsFile);
+			if (input.WorkspaceFlag.IsNotEmpty())
+			{
+				Console.WriteLine("Using workspace " + input.WorkspaceFlag);
+				runner.Workspace = input.WorkspaceFlag;
+			}
+
+			return runner.Execute() == 0;
+		}
+	}
+}
