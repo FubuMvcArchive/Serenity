16374252a67d4b0d8c793460ed317118bd64a8c8
jeremydmiller <jeremydmiller@yahoo.com>
2013-05-21 09:46:52 -0500

    converting to FubuRake.   Closes GH-44

--- rakefile.rb
+++ rakefile.rb
@@ -1,124 +1,38 @@
-COMPILE_TARGET = ENV['config'].nil? ? "Debug" : ENV['config'] # Keep this in sync w/ VS settings since Mono is case-sensitive
-CLR_TOOLS_VERSION = "v4.0.30319"
-
-buildsupportfiles = Dir["#{File.dirname(__FILE__)}/buildsupport/*.rb"]
-
-if( ! buildsupportfiles.any? )
-  # no buildsupport, let's go get it for them.
-  sh 'git submodule update --init' unless buildsupportfiles.any?
-  buildsupportfiles = Dir["#{File.dirname(__FILE__)}/buildsupport/*.rb"]
+begin
+  require 'bundler/setup'
+  require 'fuburake'
+rescue LoadError
+  puts 'Bundler and all the gems need to be installed prior to running this rake script. Installing...'
+  system("gem install bundler --source http://rubygems.org")
+  sh 'bundle install'
+  system("bundle exec rake", *ARGV)
+  exit 0
 end
 
-# nope, we still don't have buildsupport. Something went wrong.
-raise "Run `git submodule update --init` to populate your buildsupport folder." unless buildsupportfiles.any?
 
-buildsupportfiles.each { |ext| load ext }
-
-include FileTest
-require 'albacore'
-load "VERSION.txt"
-
-RESULTS_DIR = "results"
-PRODUCT = "Serenity"
-COPYRIGHT = 'Copyright 2011-2012 Jeremy D. Miller, Joshua Arnold, et al. All rights reserved.';
-COMMON_ASSEMBLY_INFO = 'src/CommonAssemblyInfo.cs';
-BUILD_DIR = File.expand_path("build")
-ARTIFACTS = File.expand_path("artifacts")
-
-@teamcity_build_id = "bt24"
-tc_build_number = ENV["BUILD_NUMBER"]
-build_revision = tc_build_number || Time.new.strftime('5%H%M')
-BUILD_NUMBER = "#{BUILD_VERSION}.#{build_revision}"
-
-props = { :stage => BUILD_DIR, :artifacts => ARTIFACTS }
-
-desc "**Default**, compiles and runs tests"
-task :default => [:compile, :unit_test, :run_jasmine]
-
-desc "Target used for the CI server"
-task :ci => [:clean, :compile, :history, :package]
-
-desc "Target used for the CI server (Mono)"
-task :mono_ci => [:clean, :compile, :history]
-
-desc "Update the version information for the build"
-assemblyinfo :version do |asm|
-  asm_version = BUILD_VERSION + ".0"
-  
-  begin
-    commit = `git log -1 --pretty=format:%H`
-  rescue
-    commit = "git unavailable"
-  end
-  puts "##teamcity[buildNumber '#{BUILD_NUMBER}']" unless tc_build_number.nil?
-  puts "Version: #{BUILD_NUMBER}" if tc_build_number.nil?
-  asm.trademark = commit
-  asm.product_name = PRODUCT
-  asm.description = BUILD_NUMBER
-  asm.version = asm_version
-  asm.file_version = BUILD_NUMBER
-  asm.custom_attributes :AssemblyInformationalVersion => asm_version
-  asm.copyright = COPYRIGHT
-  asm.output_file = COMMON_ASSEMBLY_INFO
-end
-
-desc "Prepares the working directory for a new build"
-task :clean => [:update_buildsupport] do
+@solution = FubuRake::Solution.new do |sln|
+	sln.compile = {
+		:solutionfile => 'src/Serenity.sln'
+	}
+				 
+	sln.assembly_info = {
+		:product_name => "Serenity",
+		:copyright => 'Copyright 2011-2013 Jeremy D. Miller, Josh Arnold, et al. All rights reserved.'
+	}
 	
-	FileUtils.rm_rf props[:stage]
-    # work around nasty latency issue where folder still exists for a short while after it is removed
-    waitfor { !exists?(props[:stage]) }
-	Dir.mkdir props[:stage]
-    
-	FileUtils.rm_rf props[:artifacts]
-    # work around nasty latency issue where folder still exists for a short while after it is removed
-    waitfor { !exists?(props[:artifacts]) }
-	Dir.mkdir props[:artifacts]
-end
-
-def waitfor(&block)
-  checks = 0
-  until block.call || checks >10 
-    sleep 0.5
-    checks += 1
-  end
-  raise 'waitfor timeout expired' if checks > 10
-end
-
-desc "Packages the Serenity bottle files"
-task :bottle_serenity do
-  bottles("assembly-pak src/Serenity")
-end
-
-desc "Compiles the app"
-task :compile => [:restore_if_missing, :clean, :version, :bottle_serenity] do
-  MSBuildRunner.compile :compilemode => COMPILE_TARGET, :solutionfile => 'src/Serenity.sln', :clrversion => CLR_TOOLS_VERSION
-end
-
-def copyOutputFiles(fromDir, filePattern, outDir)
-  Dir.glob(File.join(fromDir, filePattern)){|file| 		
-	copy(file, outDir, :preserve => true) if File.file?(file)
-  } 
-end
-
-desc "Runs unit tests"
-task :test => [:unit_test]
-
-desc "Runs unit tests"
-task :unit_test => :compile do
-  runner = NUnitRunner.new :compilemode => COMPILE_TARGET, :source => 'src', :platform => 'x86'
-  runner.executeTests ['Serenity.Testing']
+	sln.ripple_enabled = true
+	sln.fubudocs_enabled = true
+	
+	sln.assembly_bottle 'Serenity'
+	
+	sln.defaults = [:run_jasmine]
 end
 
 desc "Try out JasmineRunner"
 task :run_jasmine => [:compile] do
-	sh "src/SerenityRunner/bin/#{COMPILE_TARGET}/SerenityRunner.exe jasmine run src/JasmineTestApplication -b Firefox"
+	sh "src/SerenityRunner/bin/#{@solution.compilemode}/SerenityRunner.exe jasmine run src/JasmineTestApplication -b Firefox"
 end
 
-def self.bottles(args)
-  bottles = Platform.runtime(Nuget.tool("Bottles", "BottleRunner.exe"))
-  sh "#{bottles} #{args}"
-end
 
 
 
