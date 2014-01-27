ee1776be1e3d59169023d8d2d46c370a0c7ef997
Matthew Smith <mtscout6@gmail.com>
2014-01-27 15:30:11 -0700

    Added Wait.Until overload for multiple conditions tied to the same timeout

--- src/Serenity/Wait.cs
+++ src/Serenity/Wait.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 
 namespace Serenity
@@ -13,6 +15,24 @@ namespace Serenity
             var clock = new Stopwatch();
             clock.Start();
 
+            return Until(condition, clock, millisecondPolling, timeoutInMilliseconds);
+        }
+
+        public static bool Until(IEnumerable<Func<bool>> conditions, int millisecondPolling = 500, int timeoutInMilliseconds = 5000)
+        {
+            if (conditions == null)
+                throw new ArgumentNullException("conditions");
+
+            var clock = new Stopwatch();
+            clock.Start();
+
+            return conditions
+                .Where(condition => !condition())
+                .All(condition => Until(condition, clock, millisecondPolling, timeoutInMilliseconds));
+        }
+
+        private static bool Until(Func<bool> condition, Stopwatch clock, int millisecondPolling, int timeoutInMilliseconds)
+        {
             while (clock.ElapsedMilliseconds < timeoutInMilliseconds)
             {
                 Thread.Yield();
