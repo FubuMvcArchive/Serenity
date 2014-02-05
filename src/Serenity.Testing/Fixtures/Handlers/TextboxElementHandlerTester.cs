c722aa5772052378770fada36fc9008f66b1abd8
Colten Rouska <colten.rouska@extendhealth.com>
2014-02-05 15:29:01 -0700

    Added tests and alternative methods on delete for textboxElementHandler

---
+++ src/Serenity.Testing/Fixtures/Handlers/TextboxElementHandlerTester.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using FubuCore;
+using FubuCore.Descriptions;
+using FubuTestingSupport;
+using HtmlTags;
+using HtmlTags.Extended.Attributes;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using Serenity.Fixtures;
+using Serenity.Fixtures.Handlers;
+using StoryTeller;
+using StoryTeller.Domain;
+using StoryTeller.Engine;
+using TestContext = StoryTeller.Engine.TestContext;
+
+namespace Serenity.Testing.Fixtures.Handlers
+{
+    [TestFixture]
+    public class TextboxElementHandlerTester
+    {
+        private IWebDriver theDriver;
+        private IWebElement textbox1;
+        private TextboxElementHandler theHandler = new TextboxElementHandler();
+
+        private string file = "textboxfixture.htm";
+        private string theText = "(100)444-9898";
+        private string id = "textbox1";
+
+        [SetUp]
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            var document = new HtmlDocument();
+
+            document.Add(new HtmlTag("input", tag =>
+            {
+                tag.Value(theText);
+                tag.Id(id);
+            }));
+
+            document.WriteToFile(file);
+
+            try
+            {
+                StartDriver();
+            }
+            catch (Exception)
+            {
+                Thread.Sleep(2000);
+                StartDriver();
+            }
+
+            textbox1 = theDriver.FindElement(By.Id(id));
+        }
+
+        public void StartDriver()
+        {
+            theDriver = BrowserForTesting.Driver;
+
+            BrowserForTesting.Driver.Navigate().GoToUrl("file:///" + file.ToFullPath());
+        }
+
+
+        [Test]
+        public void should_be_able_to_clear_original_text()
+        {
+            theHandler.EraseData(null, textbox1);
+            theHandler.GetData(null, textbox1).ShouldBeEmpty();
+        }
+
+        [Test]
+        public void should_be_able_to_get_text_from_field()
+        {
+            Debug.WriteLine(theHandler.GetData(null, textbox1));
+            theHandler.GetData(null, textbox1).ShouldEqual(theText);
+        }
+
+        [Test]
+        public void should_be_able_to_write_to_a_clean_field()
+        {
+            var input = "Hello There";
+            theHandler.EnterData(null, textbox1, input);
+            theHandler.GetData(null, textbox1).ShouldEqual(input);
+        }
+    }
+}
