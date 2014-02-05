76c96c8c59ac7718d4f4a653898a427131ee32a2
Colten Rouska <colten.rouska@extendhealth.com>
2014-02-05 16:04:06 -0700

    Refactored the TextboxElementTester to conform to recent changes.

--- src/Serenity.Testing/Fixtures/Handlers/TextboxElementHandlerTester.cs
+++ src/Serenity.Testing/Fixtures/Handlers/TextboxElementHandlerTester.cs
@@ -1,87 +1,51 @@
-﻿using System;
-using System.Diagnostics;
-using System.Threading;
-using FubuCore;
-using FubuCore.Descriptions;
-using FubuTestingSupport;
+﻿using FubuTestingSupport;
 using HtmlTags;
 using HtmlTags.Extended.Attributes;
 using NUnit.Framework;
 using OpenQA.Selenium;
-using Serenity.Fixtures;
 using Serenity.Fixtures.Handlers;
-using StoryTeller;
-using StoryTeller.Domain;
-using StoryTeller.Engine;
-using TestContext = StoryTeller.Engine.TestContext;
 
 namespace Serenity.Testing.Fixtures.Handlers
 {
-    [TestFixture]
-    public class TextboxElementHandlerTester
+    public class TextboxElementHandlerTester : ScreenManipulationTester
     {
-        private IWebDriver theDriver;
-        private IWebElement textbox1;
-        private TextboxElementHandler theHandler = new TextboxElementHandler();
+        private readonly TextboxElementHandler _handler = new TextboxElementHandler();
 
-        private string file = "textboxfixture.htm";
-        private string theText = "(100)444-9898";
-        private string id = "textbox1";
+        private const string TheText = "(100)444-9898";
+        private const string Id = "textbox1";
+        private readonly By _byId = By.Id(Id);
 
-        [SetUp]
-        [TestFixtureSetUp]
-        public void Setup()
+        protected override void configureDocument(HtmlDocument document)
         {
-            var document = new HtmlDocument();
-
             document.Add(new HtmlTag("input", tag =>
             {
-                tag.Value(theText);
-                tag.Id(id);
+                tag.Value(TheText);
+                tag.Id(Id);
             }));
-
-            document.WriteToFile(file);
-
-            try
-            {
-                StartDriver();
-            }
-            catch (Exception)
-            {
-                Thread.Sleep(2000);
-                StartDriver();
-            }
-
-            textbox1 = theDriver.FindElement(By.Id(id));
         }
-
-        public void StartDriver()
-        {
-            theDriver = BrowserForTesting.Driver;
-
-            BrowserForTesting.Driver.Navigate().GoToUrl("file:///" + file.ToFullPath());
-        }
-
 
         [Test]
         public void should_be_able_to_clear_original_text()
         {
-            theHandler.EraseData(null, textbox1);
-            theHandler.GetData(null, textbox1).ShouldBeEmpty();
+            var textbox1 = theDriver.FindElement(_byId);
+            _handler.EraseData(null, textbox1);
+            _handler.GetData(null, textbox1).ShouldBeEmpty();
         }
 
         [Test]
         public void should_be_able_to_get_text_from_field()
         {
-            theHandler.GetData(null, textbox1).ShouldEqual(theText);
+            var textbox1 = theDriver.FindElement(_byId);
+            _handler.GetData(null, textbox1).ShouldEqual(TheText);
         }
 
         [Test]
         public void should_be_able_to_write_to_a_clean_field()
         {
-            var input = "Hello There";
-            theHandler.EnterData(null, textbox1, input);
-            theHandler.GetData(null, textbox1).ShouldEqual(input);
+            const string input = "Hello There";
+            var textbox1 = theDriver.FindElement(_byId);
+            _handler.EnterData(null, textbox1, input);
+            _handler.GetData(null, textbox1).ShouldEqual(input);
         }
     }
 }
