c97d236d76c2b121facd6a4d0d94eb41ae221226
Colten Rouska <colten.rouska@extendhealth.com>
2014-02-05 19:32:43 -0700

    Refactored TextAreaHandlerTester to conform with recent changes

--- src/Serenity.Testing/Fixtures/Handlers/TextAreaElementHandlerTester.cs
+++ src/Serenity.Testing/Fixtures/Handlers/TextAreaElementHandlerTester.cs
@@ -14,60 +14,37 @@ using Serenity.Fixtures.Handlers;
 namespace Serenity.Testing.Fixtures.Handlers
 {
     [TestFixture]
-    public class TextAreaElementHandlerTester
+    public class TextAreaElementHandlerTester : ScreenManipulationTester
     {
-        private IWebDriver theDriver;
-        private IWebElement textarea1;
-        private TextAreaElementHandler theHandler = new TextAreaElementHandler();
-        private string theText = "Test data within the textarea";
+        private readonly TextAreaElementHandler _handler = new TextAreaElementHandler();
 
-        [TestFixtureSetUp]
-        public void SetUp()
+        private const string TheText = "Test data within the textarea";
+        private const string Id = "textbox1";
+        private readonly By _byId = By.Id(Id);
+
+        protected override void configureDocument(HtmlDocument document)
         {
-            var document = new HtmlDocument();
             document.Add(new HtmlTag("textarea", tag =>
-                                                     {
-                                                         tag.Id("textarea1");
-                                                         tag.Text(theText);
-                                                     })
-                );
-
-    
-            
-
-            document.WriteToFile("textarea.htm");
-
-            try
             {
-                startDriver();
-            }
-            catch (Exception)
-            {
-                Thread.Sleep(2000);
-                startDriver();
-            }
-
-            textarea1 = theDriver.FindElement(By.Id("textarea1"));
-        }
-
-        private void startDriver()
-        {
-            theDriver = BrowserForTesting.Driver;
-            theDriver.Navigate().GoToUrl("file:///" + "textarea.htm".ToFullPath());
+                tag.Id(Id);
+                tag.Text(TheText);
+            }));
         }
 
         [Test]
         public void should_be_able_to_read_write()
         {
-            theHandler.EnterData(null, textarea1, "New Data");
-            theHandler.GetData(null, textarea1).ShouldEqual("New Data");
+            var textarea1 = theDriver.FindElement(_byId);
+            _handler.EnterData(null, textarea1, "New Data");
+            _handler.GetData(null, textarea1).ShouldEqual("New Data");
         }
 
         [Test]
         public void clearing_data_should_return_clear_and_not_original_text()
         {
-            theHandler.EnterData(null, textarea1, "");
-            theHandler.GetData(null, textarea1).ShouldEqual("");
+            var textarea1 = theDriver.FindElement(_byId);
+            _handler.EnterData(null, textarea1, "");
+            _handler.GetData(null, textarea1).ShouldEqual("");
         }
     }
 }
