969e356d73da1bbcbb82ba764d2d250c0a489022
Colten Rouska <colten.rouska@extendhealth.com>
2014-02-06 09:52:02 -0700

    Removed unused using statements for Checkbox, Select and TextArea HandlerTesters

--- src/Serenity.Testing/Fixtures/Handlers/TextAreaElementHandlerTester.cs
+++ src/Serenity.Testing/Fixtures/Handlers/TextAreaElementHandlerTester.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using FubuCore;
-using FubuTestingSupport;
+﻿using FubuTestingSupport;
 using HtmlTags;
-using HtmlTags.Extended.Attributes;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using Serenity.Fixtures.Handlers;
