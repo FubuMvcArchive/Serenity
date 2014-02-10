e505706f753c7f7f2abbb618533a38fcd5600fd0
Colten Rouska <colten.rouska@extendhealth.com>
2014-02-10 10:09:08 -0700

    Added vertual to TextboxElementHandler methods for override

--- src/Serenity/Fixtures/Handlers/TextboxElementHandler.cs
+++ src/Serenity/Fixtures/Handlers/TextboxElementHandler.cs
@@ -5,19 +5,19 @@ namespace Serenity.Fixtures.Handlers
 {
     public class TextboxElementHandler : IElementHandler
     {
-        public bool Matches(IWebElement element)
+        public virtual bool Matches(IWebElement element)
         {
             // TODO --- Change to psuedo CSS class?  Less repeated logic
             return element.TagName.ToLower() == "input" && element.GetAttribute("type").ToLower() == "text";
         }
 
-        public void EraseData(ISearchContext context, IWebElement element)
+        public virtual void EraseData(ISearchContext context, IWebElement element)
         {
             if (element.GetAttribute("value").IsNotEmpty())
                 element.SendKeys(Keys.Home + Keys.Shift + Keys.End + Keys.Delete);
         }
 
-        public void EnterData(ISearchContext context, IWebElement element, object data)
+        public virtual void EnterData(ISearchContext context, IWebElement element, object data)
         {
             if (element.GetAttribute("value").IsNotEmpty())
                 EraseData(context, element);
@@ -25,7 +25,7 @@ namespace Serenity.Fixtures.Handlers
             element.SendKeys(data as string ?? string.Empty);
         }
 
-        public string GetData(ISearchContext context, IWebElement element)
+        public virtual string GetData(ISearchContext context, IWebElement element)
         {
             return element.GetAttribute("value");
         }
