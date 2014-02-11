bc9e9fc80000e62898f2d72921331a0dace573c7
Colten Rouska <colten.rouska@extendhealth.com>
2014-02-11 10:56:06 -0700

    Added element.Click to EraseData to make sure element has focus

--- src/Serenity/Fixtures/Handlers/TextboxElementHandler.cs
+++ src/Serenity/Fixtures/Handlers/TextboxElementHandler.cs
@@ -17,7 +17,10 @@ namespace Serenity.Fixtures.Handlers
         public virtual void EraseData(ISearchContext context, IWebElement element)
         {
             if (element.GetAttribute("value").IsNotEmpty())
-                element.SendKeys(Keys.Home + Keys.Shift + Keys.End + Keys.Backspace);
+            {
+                element.Click();
+                element.SendKeys(Keys.Home + Keys.Shift + Keys.End + Keys.Delete);
+            }
         }
 
         public virtual void EnterData(ISearchContext context, IWebElement element, object data)
