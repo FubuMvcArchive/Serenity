b2d994a1192e29a672dc96ff89dce1c499d2a98a
Matthew Smith <mtscout6@gmail.com>
2014-02-11 15:14:13 -0700

    Embedded Chrome and Phantom into the Serenity dll and extract it at runtime #74

--- src/Serenity/PhantomBrowser.cs
+++ src/Serenity/PhantomBrowser.cs
@@ -2,18 +2,19 @@ using System;
 using FubuCore;
 using OpenQA.Selenium;
 using OpenQA.Selenium.PhantomJS;
+using Serenity.WebDriver.EmbeddedDrivers;
 using StoryTeller;
 
 namespace Serenity
 {
-    public class PhantomBrowser : BrowserLifecycle
+    public class PhantomBrowser : EmbeddedBrowserLifecycle<PhantomEmbeddedDriver>
     {
         public const string Process = "phantomjs";
         public const string File = "phantomjs.exe";
 
         public override string BrowserName { get { return "Phantom"; } }
 
-        protected override IWebDriver buildDriver()
+        protected override IWebDriver constructDriver()
         {
             var fileSystem = new FileSystem();
             var settings = StoryTellerEnvironment.Get<SerenityEnvironment>();
