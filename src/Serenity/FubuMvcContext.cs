4ba5c39b13f3ef19b192b138fec4a0367e30fee3
Matthew Smith <mtscout6@gmail.com>
2014-02-28 15:17:20 -0700

    Changed tabs to spaces

--- src/Serenity/FubuMvcContext.cs
+++ src/Serenity/FubuMvcContext.cs
@@ -7,40 +7,41 @@ using StoryTeller.Engine;
 
 namespace Serenity
 {
-	public class FubuMvcContext : IExecutionContext, IResultsExtension
-	{
-		private readonly IApplicationUnderTest _application;
-		private readonly BindingRegistry _binding;
-	    private readonly IEnumerable<IContextualInfoProvider> _contextualProviders;
+    public class FubuMvcContext : IExecutionContext, IResultsExtension
+    {
+        private readonly IApplicationUnderTest _application;
+        private readonly BindingRegistry _binding;
+        private readonly IEnumerable<IContextualInfoProvider> _contextualProviders;
 
-	    public FubuMvcContext(IApplicationUnderTest application, BindingRegistry binding, IEnumerable<IContextualInfoProvider> contextualProviders )
-		{
-			_application = application;
-			_binding = binding;
+        public FubuMvcContext(IApplicationUnderTest application, BindingRegistry binding,
+            IEnumerable<IContextualInfoProvider> contextualProviders)
+        {
+            _application = application;
+            _binding = binding;
 
-		    _contextualProviders = contextualProviders ?? new IContextualInfoProvider[0];
+            _contextualProviders = contextualProviders ?? new IContextualInfoProvider[0];
 
-	        _contextualProviders.Each(x => x.Reset());
-		}
+            _contextualProviders.Each(x => x.Reset());
+        }
 
-		public void Dispose()
-		{
+        public void Dispose()
+        {
 
-		}
+        }
 
-		public IServiceLocator Services
-		{
-			get { return _application.Services; }
-		}
+        public IServiceLocator Services
+        {
+            get { return _application.Services; }
+        }
 
-		public BindingRegistry BindingRegistry
-		{
-			get { return _binding; }
-		}
+        public BindingRegistry BindingRegistry
+        {
+            get { return _binding; }
+        }
 
-	    public IEnumerable<HtmlTag> Tags()
-	    {
-	        return _contextualProviders.SelectMany(x => x.GenerateReports());
-	    }
-	}
+        public IEnumerable<HtmlTag> Tags()
+        {
+            return _contextualProviders.SelectMany(x => x.GenerateReports());
+        }
+    }
 }
