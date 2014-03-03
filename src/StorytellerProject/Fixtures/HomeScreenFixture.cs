aa945500ef3e7b49471802b371d57807d5c05315
Matthew Smith <mtscout6@gmail.com>
2014-03-03 11:32:30 -0700

    Added failing test that proves the app is not recycling on test retries

--- src/StorytellerProject/Fixtures/HomeScreenFixture.cs
+++ src/StorytellerProject/Fixtures/HomeScreenFixture.cs
@@ -22,5 +22,25 @@ namespace StorytellerProject.Fixtures
             waitForElement(By.Name("foo"));
             return Driver.FindElement(By.Name("foo")).GetAttribute("value");
         }
+
+        [FormatAs("Can find elements on screen")]
+        public bool CanFindElements()
+        {
+            try
+            {
+                Driver.FindElement(By.Name("foo"));
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        [FormatAs("Only succeed on/after test attempt {attempt}")]
+        public bool OnlyPassOnAttempt(int attempt)
+        {
+            return Retrieve<ITestContext>().RetryAttemptNumber >= attempt;
+        }
     }
 }
